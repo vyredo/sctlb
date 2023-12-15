362530ba1de74b2ae98534570b5de8f28efe0361
vidy <vidy.alfredo1@gmail.com>
2023-12-15 23:16:07 +0800

    add infinite loader

---
+++ src/app/shared_components/InfiniteLoader/InfiniteLoader.tsx
@@ -0,0 +1,34 @@
+import { Spinner } from "@/assets/Spinner/Spinner";
+import { throttle } from "@/lib/throttle";
+import React, { useEffect, useRef } from "react";
+
+export const InfiniteLoader: React.FC<{ loadFunction: () => Promise<any> }> = ({ loadFunction }) => {
+  const divRef = React.useRef<HTMLDivElement>(null);
+
+  const throttleFunc = useRef(throttle(2000, loadFunction));
+  useEffect(() => {
+    // use Observable
+    // use IntersectionObserver
+    // use scroll event
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries[0].intersectionRatio <= 0) return;
+      // load more content;
+      throttleFunc.current();
+      console.log("load more content");
+    });
+    // start observing
+    if (divRef.current) {
+      intersectionObserver.observe(divRef.current);
+    }
+
+    return () => {
+      intersectionObserver.disconnect();
+    };
+  }, [loadFunction]);
+
+  return (
+    <div ref={divRef} className="infinite-loader">
+      <Spinner />
+    </div>
+  );
+};
