6ff54595e93144781ff6972a9d5f3806f5cd2189
vidy <vidy.alfredo1@gmail.com>
2023-12-16 13:02:00 +0800

    adding test with coverage

---
+++ README copy.md
@@ -0,0 +1,24 @@
+## Note
+
+Hey there! Task completed! 🎉
+The task are quite big especially since I have to create several component from scratch and have to create unittest for each component that I created.
+I decide to keep design simple to save time.
+Focused on key features for ya! Let me know what you think. 😊
+
+## Vercel endpoint
+
+## How to run locally
+
+```bash
+git clone
+cd sctlb
+npm run dev
+```
+
+## How to run test with coverage
+
+```bash
+npm run test
+```
+
+## Bonus
--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 const { defaults } = require("jest-config");
 
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "jsdom",
   modulePathIgnorePatterns: ["dist/"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
@@ -8,11 +9,19 @@ module.exports = {
     "^.+\\.(css|less|scss)$": "identity-obj-proxy",
     "@/(.*)": "<rootDir>/src/$1",
   },
-  //   setupFilesAfterEnv: ["./setupTests.ts"],
-  coverageReporters: ["text", "lcov"],
-  coverageDirectory: ".",
   transform: {
     ".(js|jsx)": "babel-jest",
     ".(ts|tsx)": "ts-jest",
+  },
+  coverageReporters: ["text", "lcov", "html"],
+  coverageDirectory: "./jest-coverage",
+  collectCoverage: true,
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  roots: ["<rootDir>/src"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.test.json",
+    },
   },
 };
